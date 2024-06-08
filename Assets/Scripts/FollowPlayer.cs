785ff2d64632110195d4599de1e07eecda6b1a28
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-06-08 12:44:24 +0900

    Make the offset into a Vector3 variable

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
