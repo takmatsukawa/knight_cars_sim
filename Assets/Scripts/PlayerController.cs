03b0cc36fba852411b0df670b488712e483e1303
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-06-08 13:01:47 +0900

    Allow the vehicle to move left/right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 40;
+    public float turnSpeed;
 
     void Update()
     {
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
