14e7bb8d99e60cd4321933bc8c5da1980406c65f
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-06-08 12:39:00 +0900

    Add an offset to the camera position

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -9,6 +9,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
