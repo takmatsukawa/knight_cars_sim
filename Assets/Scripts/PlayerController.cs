51051e5c1feb97a9cd74bf47ad190308b47da40e
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-06-08 13:17:35 +0900

    Take control of the vehicle speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,15 @@ public class NewBehaviourScript : MonoBehaviour
     public float speed = 40;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
